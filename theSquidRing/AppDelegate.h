12ce900a652cab96674f865a2905799febe072cd
zhx <zhx@chans-MacBook-Pro-4.local>
2015-04-21 09:28:32 +0800

    Initial Commit

---
+++ theSquidRing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  theSquidRing
+//
+//  Created by zhx on 15/4/21.
+//  Copyright (c) 2015年 zhx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ theSquidRing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  theSquidRing
+//
+//  Created by zhx on 15/4/21.
+//  Copyright (c) 2015年 zhx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
